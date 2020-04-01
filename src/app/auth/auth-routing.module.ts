a77bb4642ed7cacf152fcd7d39d4ff3c94ec406b
Deepak Kumar <deekumar27@publicisgroupe.net>
2020-04-01 20:13:57 +0530

    kt

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,13 +3,11 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
-import { PageNotFoundComponent } from '../core/components/page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: '', redirectTo: 'signin', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '', redirectTo: 'signin', pathMatch: 'full' }
 ];
 
 @NgModule({
